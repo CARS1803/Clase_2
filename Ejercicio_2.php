8293adf4eaa3ae6733fda02e0c2318c37dc3ed68
Reyes <Carlosreyes1803@gmail.com>
2023-05-04 13:33:07 -0600

    Ejercicio 2 de POO

---
+++ Ejercicio_2.php
@@ -0,0 +1,36 @@
+<?php
+class Producto{
+    #DEFINICION DE LOS ATRIBUTOS
+    protected string $nombre;
+    public int $precio;
+    public bool $disponible;
+
+    #METODO CONSTRUCTOR, SE EJECUTA AL EFECTUAR LA INSTANCIA
+    public function __construct(string $nombre, int $precio, bool $disponible)
+    {
+        #SE LES DA VALORES A LOS ATRIBUTOS CON LOS PARAMETROS QUE RECIBE EL METODO CONSTRUCTOR
+        $this->nombre=$nombre;
+        $this->precio=$precio;
+        $this->disponible=$disponible;
+    }
+
+    #DEFINICION DE METODOS
+    public function mostrarProducto(){
+        echo 'El producto es: '.$this->nombre. ' y su precio es de: ' .$this->precio;
+    }
+    #METODO GETTER PARA LA PROPIEDAD PROTEGIDA DE NOMBRE
+    public function getNombre() : string {
+        return $this->nombre;
+    }
+    #METODO SETTER PAR APROPIEDAD PROTEGIDA DE NOMBRE
+    public function setNombre(string $nombre){
+        $this->nombre=$nombre;
+    }
+}
+
+$producto=new Producto('tablet', 200, true);
+//$producto->mostrarProducto();
+echo $producto->nombre; //PRODUCE UN ERROR
+echo $producto->getNombre().'<bR><bR>'; //OBTIENE EL NOMBRE
+$producto->setNombre('Nuevo Nombre'); //SETEA EL NOMBRE
+echo $producto->getNombre(); //OBTIENE EL NOMBRE
